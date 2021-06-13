5754cc7aa8e38cf25332aa2ee11cc01578f9ac68
Третьяк Глеб Игоревич <60621582+VasiliyWebDesign@users.noreply.github.com>
2021-06-13 14:59:34 +0300

    Add files via upload

---
+++ 14/bd.php
@@ -0,0 +1,6 @@
+<?php
+    $dbloc = 'localhost';
+    $dbuser = $dbname = 'user1';
+    $dbpass = 'Qwerty123';
+    $dsn = $dsn = "mysql:host={$dbloc};dbname={$dbname}";
+    return new PDO($dsn, $dbuser, $dbpass);
